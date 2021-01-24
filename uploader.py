9d9d74e6930e0b4ca682245cff1f8114d8ea9a2b
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2021-01-24 12:35:32 -0300

    sending all the .sql files that are inside the uploader folder

--- uploader.py
+++ uploader.py
@@ -1,30 +1,39 @@
 #!/usr/bin/env python3
 
-import sys
 import getopt
-import dropbox
+import glob
+import os
+import sys
 
+import dropbox
 from decouple import config
+
+DEFAULT_SQL_DIRECTORY = config("DEFAULT_SQL_DIRECTORY")
+DEFAULT_DROPBOX_DIRECTORY = config("DEFAULT_DROPBOX_DIRECTORY")
+
+
+def get_files_sql():
+    os.chdir(fr'{DEFAULT_SQL_DIRECTORY}')
+    return glob.glob("*.sql")
 
 
 class TransferData:
     def __init__(self, access_token):
         self.access_token = access_token
 
-    def upload_file(self, file_from, file_to):
+    def upload_file(self, file_from, directory):
         """upload a file to Dropbox using API v2
         """
         dbx = dropbox.Dropbox(self.access_token)
 
         with open(file_from, 'rb') as f:
-            dbx.files_upload(f.read(), file_to, mode=dropbox.files.WriteMode.overwrite)
+            dbx.files_upload(f.read(), directory, mode=dropbox.files.WriteMode.overwrite)
 
 
 def main(argv):
-    file_from = ''
-    file_to = ''
+    file_from = directory = None
     try:
-        opts, args = getopt.getopt(argv,"hf:d:",["file=","directory="])
+        opts, args = getopt.getopt(argv, "hf:d:", ["file=", "directory="])
     except getopt.GetoptError:
         print('uploader.py -f <file_name> -d <dropbox_directory>')
         sys.exit(2)
@@ -35,12 +44,16 @@ def main(argv):
         elif opt in ("-f", "--file"):
             file_from = arg
         elif opt in ("-d", "--directory"):
-            file_to = arg
-    
+            directory = arg
+
     access_token = config('ACCESS_TOKEN')
     transferData = TransferData(access_token)
-
-    transferData.upload_file(file_from, file_to)
+    if not file_from and not directory:
+        for file in get_files_sql():
+            directory = f"{DEFAULT_DROPBOX_DIRECTORY}{file.split('/')[-1]}"
+            transferData.upload_file(file, directory)
+    else:
+        transferData.upload_file(file_from, directory)
 
 
 if __name__ == '__main__':
