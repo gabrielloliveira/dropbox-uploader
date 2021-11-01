0f94721a91454110b563632d4415ad9f658abf1d
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2021-11-01 14:34:54 -0300

    add option -c to clear folder first

--- uploader.py
+++ uploader.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import getopt
 import glob
 import os
@@ -33,12 +34,24 @@ class DropboxUploader:
             if not err.error.is_path() or err.error.get_path().is_not_found():
                 self.client.files_create_folder(directory)
 
-    def upload_list_files(self, files, dropbox_directory):
+    def clear_directory(self, dropbox_directory):
+        dropbox_directory = (
+            dropbox_directory[:-1]
+            if dropbox_directory.endswith("/")
+            else dropbox_directory
+        )
+        print("🗑 Clearing directory " + dropbox_directory + "...")
+        self.client.files_delete_v2(dropbox_directory)
+
+    def upload_list_files(self, files, dropbox_directory, clear_directory=False):
         self.create_directory_if_not_exists(dropbox_directory)
+        if clear_directory:
+            self.clear_directory(dropbox_directory)
         for file in files:
             full_directory = fr"{dropbox_directory}{file}"
-            print("Uploading " + file + " to Dropbox as " + full_directory + "...")
+            print("🔄 Uploading " + file + " to Dropbox as " + full_directory + "...")
             self.upload(file, full_directory)
+            print("✅ Success " + full_directory)
 
     def upload(self, file, dropbox_directory):
         with open(file, "rb") as f:
@@ -48,21 +61,28 @@ class DropboxUploader:
 
 
 def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", help="File", required=False)
+    parser.add_argument("-d", "--directory", help="Dropbox directory", required=False)
+    parser.add_argument(
+        "-c",
+        "--clear",
+        help="Clear Dropbox directory first",
+        required=False,
+        action="store_true",
+    )
+    args = parser.parse_args()
+
     file_list = get_files_sql()
     directory = DEFAULT_DROPBOX_DIRECTORY
-    try:
-        opts, args = getopt.getopt(argv, "hf:d:", ["file=", "directory="])
-    except getopt.GetoptError:
-        print("uploader.py -f <file_name> -d <dropbox_directory>")
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == "-h":
-            print("uploader.py -f <file_name> -d <dropbox_directory>")
-            sys.exit()
-        elif opt in ("-f", "--file"):
-            file_list = arg.split(",")
-        elif opt in ("-d", "--directory"):
-            directory = arg
+    clear = False
+
+    if args.file:
+        file_list = args.file.split(",")
+    if args.directory:
+        directory = args.directory
+    if args.clear:
+        clear = True
 
     if not directory.endswith("/"):
         directory += "/"
@@ -70,7 +90,7 @@ def main(argv):
         directory = "/" + directory
 
     transfer_data = DropboxUploader()
-    transfer_data.upload_list_files(file_list, directory)
+    transfer_data.upload_list_files(file_list, directory, clear_directory=clear)
 
 
 if __name__ == "__main__":
